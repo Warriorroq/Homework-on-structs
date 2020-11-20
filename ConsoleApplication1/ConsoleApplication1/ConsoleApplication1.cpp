f0dea3274dd4af93ab309eb5c7182c5bca2b9425
Warriorroq <Warriorroq@gmail.com>
2020-11-20 13:29:11 +0200

    Created some stuff (need to end coping of vectr)

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,13 +7,36 @@ using namespace std;
 
 struct Vector3 {
 public:
-
 	double x;
 	double y;
 	double z;
 	Vector3(double x = 0, double y = 0, double z = 0, string name = "") : x(x),y(y),z(z),name(name) {
 		if (name.length() == 0)
 			SetRandomName();
+	}
+
+	virtual void operator +=(Vector3 & vec) {
+		x += vec.x;
+		y += vec.y;
+		z += vec.z;
+	}
+
+	virtual void operator -=(Vector3 & vec) {
+		x -= vec.x;
+		y -= vec.y;
+		z -= vec.z;
+	}	
+
+	virtual void operator *=(double vec) {
+		x *= vec;
+		y *= vec;
+		z *= vec;
+	}
+
+	virtual void operator /=(double vec) {
+		x /= vec;
+		y /= vec;
+		z /= vec;
 	}
 
 	double Length() {
@@ -33,6 +56,14 @@ public:
 	void SetName(string name) {
 		this->name = name;
 	}
+
+	void PrintInfo() {
+		cout << "Name : " << name << endl;
+		cout << "Vector :" <<"(" << x << "," << y << "," << z << ")" << endl;
+		cout << "Lenght : " << Length() << endl;
+
+	}
+
 private: 
 	string name;
 	void SetRandomName() {
@@ -42,7 +73,19 @@ private:
 
 int main()
 {
-	Vector3 vec(1, 1, 1);
-    cout << "Hello World! " << vec.GetName() << endl;
+	Vector3 * vecs = (Vector3 *)malloc(sizeof(Vector3) * 100);
+	for (int i = 0; i < 100; i++)
+		vecs[i] = Vector3(rand() % 10, rand() % 5, rand() % 18);
+
+	while (true){
+		int a = 0;
+		cin >> a;
+		if (a >= 0)
+			if(a < 100)
+				vecs[a].PrintInfo();
+		else
+			return false;
+	}
+
 	return 0;
 }
