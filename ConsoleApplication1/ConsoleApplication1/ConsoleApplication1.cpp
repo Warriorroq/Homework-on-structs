d9b06805a078009e9c12d84c00fd611ddd800055
Warriorroq <Warriorroq@gmail.com>
2020-11-20 12:31:16 +0200

    Created struct

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,17 @@
-﻿
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+struct Vector3 {
+public:
+	double x;
+	double y;
+	double z;
+	Vector3(double x = 0, double y = 0, double z = 0, double scale = 0, string name = "") : x(x),y(y),z(z) {
+
+	}
+
+};
 
 int main()
 {
